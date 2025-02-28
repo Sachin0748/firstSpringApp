6fd1d5aa00c8a94102f94a14b656e1492e94fda0
Sachin Kumar Thakur <kumarsachinsk456@gmail.com>
2025-02-28 14:24:26 +0530

    [Sachin] Add . firstSpringApp UC_02

--- src/main/java/com/firstspringapp/HelloWorldController.java
+++ src/main/java/com/firstspringapp/HelloWorldController.java
@@ -3,6 +3,7 @@ package com.firstspringapp;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.Model;
 
 @RestController
 @RequestMapping("/hello")
@@ -10,7 +11,14 @@ public class HelloWorldController {
 
     // UC_01
     @GetMapping("/hello1")
-    public String sayHello(){
+    public String sayHello1(){
         return "Hello from BridgeLabz";
+    }
+
+    // UC_02
+    @GetMapping("/hello2")
+    public String sayHello2(Model model) {
+        model.addAttribute("message", "Hello from BridgeLabz");
+        return "hello"; // This refers to the Thymeleaf template (hello.html)
     }
 }
